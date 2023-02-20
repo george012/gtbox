255a33671b606b801444a9484676c1a3b945f502
WMYeah_Mac <george012@163.com>
2023-02-20 19:52:52 +0800

    Update v0.0.16

--- install_gtbox.sh
+++ install_gtbox.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 ProductName=gtbox
-#CustomLibs="gtgo,otherlib1,otherlib2"
-CustomLibs=$(ls -l ./libs|awk '/^d/ {print $NF}')
-echo $CustomLibs
-
 OSTYPE="Unknown"
 GetOSType(){
     uNames=`uname -s`
@@ -43,37 +39,42 @@ install() {
         find ${GOPATH}/pkg/mod/github.com/george012  -name "${ProductName}@*" -exec rm -rf {} \;
     fi
 
-    wget --no-check-certificate https://raw.githubusercontent.com/george012/${ProductName}/master/version.go -O ${ProductName}_version.go
-    aVersion=`cat ./gtbox_version.go | grep -n "const VERSION =" | awk -F ":" '{print $2}'`
-    aVersionString=`echo "${aVersion/'const VERSION = '/}" | sed 's/\"//g'`
-    aVersionNo=`echo "${aVersionString}" | awk -F "v" '{print $2}'`
 
-    for alibName in ${CustomLibs}
-    do
-        if [ ${OSTYPE} == "Darwin" ] # Darwin
-        then
-            srcPWD=`pwd`
-            echo "handle Lib = "${alibName}
-    #        cd ${GOPATH}/pkg/mod/github.com/george012/gtbox@v${aVersionNo} && /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/install_name_tool -add_rpath ../gtbox@v${aVersionNo} ${produckName} && cd ${srcPWD}
-            ln -s ${GOPATH}/pkg/mod/github.com/george012/gtbox@v$aVersionNo/libs/${alibName}/lib$libName.dylib /usr/local/lib/lib${alibName}.dylib
-            ln -s /usr/local/lib/lib${alibName}.dylib /usr/local/lib/lib${alibName}_arm64.dylib
-        elif [ ${OSTYPE} == "Linux" ] # Linux
-        then
-            ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v$aVersionNo/libs/${alibName}/lib${alibName}.so /lib64/lib${alibName}.so && ldconfig
-        elif [ ${OSTYPE} == "Windows" ] # MINGW, windows, git-bash
-        then
-            ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v$aVersionNo/libs/${alibName}/${alibName}.dll /c/Windows/System32/${alibName}.dll
-        else
-            echo ${OSTYPE}
-        fi
-    done
+
+    wget --no-check-certificate https://raw.githubusercontent.com/george012/${ProductName}/master/version.go -O ${ProductName}_version.go \
+    && {
+
+        aVersion=`cat ./${ProductName}_version.go | grep -n "const VERSION =" | awk -F ":" '{print $2}'` \
+        && aVersionString=`echo "${aVersion/'const VERSION = '/}" | sed 's/\"//g'` \
+        && aVersionNo=`echo "${aVersionString}" | awk -F "v" '{print $2}'` \
+        && CustomLibs=$(ls -l ${GOPATH}/pkg/mod/github.com/george012/gtbox@v$aVersionNo/libs |awk '/^d/ {print $NF}') \
+        && for alibName in ${CustomLibs}
+        do
+            echo "libs cons = "${CustomLibs}
+            echo "handle lib ="${alibName}
+            if [ ${OSTYPE} == "Darwin" ] # Darwin
+            then
+                srcPWD=`pwd`
+        #        cd ${GOPATH}/pkg/mod/github.com/george012/gtbox@v${aVersionNo} && /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/install_name_tool -add_rpath ../gtbox@v${aVersionNo} ${produckName} && cd ${srcPWD}
+                ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/lib${alibName}.dylib /usr/local/lib/lib${alibName}.dylib
+                ln -s /usr/local/lib/lib${alibName}.dylib /usr/local/lib/lib${alibName}_arm64.dylib
+            elif [ ${OSTYPE} == "Linux" ] # Linux
+            then
+                ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/lib${alibName}.so /lib64/lib${alibName}.so && ldconfig
+            elif [ ${OSTYPE} == "Windows" ] # MINGW, windows, git-bash
+            then
+                ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/${alibName}.dll /c/Windows/System32/${alibName}.dll
+            else
+                echo ${OSTYPE}
+            fi
+        done
+    }
 
 #    removeCache
 }
 
 uninstall() {
     GetOSType
-    echo ${OSTYPE}
     for libName in ${CustomLibs}
     do
         if [ ${OSTYPE} == "Darwin" ] # Darwin
