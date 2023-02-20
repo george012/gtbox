0f62a188e874abb69dec2677277b44d13043bf4c
WMYeah_Mac <george012@163.com>
2023-02-21 06:26:50 +0800

    Update v0.0.30

--- gtbox_map/gtbox_map.go
+++ gtbox_map/gtbox_map.go
@@ -1,6 +1,6 @@
 package gtbox_map
 
-func GTMapGetAllKeys(m map[string]any) []string {
+func GTMapGetAllKeys(m map[string]interface{}) []string {
 	// 数组默认长度为map长度,后面append时,不需要重新申请内存和拷贝,效率很高
 	keys := make([]string, 0, len(m))
 	for k := range m {
