b23c71085bfc4e898d9107135b4394de22198844
WMYeah-NUC <george012@163.com>
2023-02-22 15:33:40 +0800

    Update v0.0.33

--- install_gtbox.sh
+++ install_gtbox.sh
@@ -24,8 +24,7 @@ removeCache() {
 }
 
 install() {
-
-    go get -u github.com/george012/${ProductName}@latest
+    complate_gopath_dir=${GOPATH}
 
     GetOSType
     echo ${OSTYPE}
@@ -35,10 +34,10 @@ install() {
         complate_gopath_dir='/'`echo "${ago_path_dir}" | tr A-Z a-z`
         find $complate_gopath_dir/pkg/mod/github.com/george012  -name "${ProductName}@*" -exec rm -rf {} \;
     else
-        find ${GOPATH}/pkg/mod/github.com/george012  -name "${ProductName}@*" -exec rm -rf {} \;
+        find ${complate_gopath_dir}/pkg/mod/github.com/george012  -name "${ProductName}@*" -exec rm -rf {} \;
     fi
 
-
+    go get -u github.com/george012/${ProductName}@latest
 
     wget --no-check-certificate https://raw.githubusercontent.com/george012/${ProductName}/master/version.go -O ${ProductName}_version.go \
     && {
@@ -46,21 +45,21 @@ install() {
         aVersion=`cat ./${ProductName}_version.go | grep -n "const VERSION =" | awk -F ":" '{print $2}'` \
         && aVersionString=`echo "${aVersion/'const VERSION = '/}" | sed 's/\"//g'` \
         && aVersionNo=`echo "${aVersionString}" | awk -F "v" '{print $2}'` \
-        && CustomLibs=$(ls -l ${GOPATH}/pkg/mod/github.com/george012/gtbox@v$aVersionNo/libs |awk '/^d/ {print $NF}') \
+        && CustomLibs=$(ls -l ${complate_gopath_dir}/pkg/mod/github.com/george012/gtbox@v$aVersionNo/libs |awk '/^d/ {print $NF}') \
         && for alibName in ${CustomLibs}
         do
             if [ ${OSTYPE} == "Darwin" ] # Darwin
             then
                 srcPWD=`pwd`
         #        cd ${GOPATH}/pkg/mod/github.com/george012/gtbox@v${aVersionNo} && /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/install_name_tool -add_rpath ../gtbox@v${aVersionNo} ${produckName} && cd ${srcPWD}
-                ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/lib${alibName}.dylib /usr/local/lib/lib${alibName}.dylib
+                ln -s ${complate_gopath_dir}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/lib${alibName}.dylib /usr/local/lib/lib${alibName}.dylib
                 ln -s /usr/local/lib/lib${alibName}.dylib /usr/local/lib/lib${alibName}_arm64.dylib
             elif [ ${OSTYPE} == "Linux" ] # Linux
             then
-                ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/lib${alibName}.so /lib64/lib${alibName}.so && ldconfig
+                ln -s ${complate_gopath_dir}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/lib${alibName}.so /lib64/lib${alibName}.so && ldconfig
             elif [ ${OSTYPE} == "Windows" ] # MINGW, windows, git-bash
             then
-                ln -s ${GOPATH}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/${alibName}.dll /c/Windows/System32/${alibName}.dll
+                ln -s ${complate_gopath_dir}/pkg/mod/github.com/george012/${ProductName}@v${aVersionNo}/libs/${alibName}/${alibName}.dll /c/Windows/System32/${alibName}.dll
             else
                 echo ${OSTYPE}
             fi
@@ -71,6 +70,7 @@ install() {
 }
 
 uninstall() {
+    complate_gopath_dir=${GOPATH}
     GetOSType
     for libName in ${CustomLibs}
     do
@@ -83,13 +83,15 @@ uninstall() {
             rm -rf /lib64/lib${libName}.so
         elif [ ${OSTYPE} == "Windows" ] # MINGW, windows, git-bash
         then
+            ago_path_dir=`echo "${GOPATH/':\\'/'/'}" | sed 's/\"//g'`
+            complate_gopath_dir='/'`echo "${ago_path_dir}" | tr A-Z a-z`
             rm -rf c:/Windows/System32/${libName}.dll
         else
             echo ${OSTYPE}
         fi
     done
     removeCache
-    find ${GOPATH}/pkg/mod/github.com/george012  -name "${ProductName}@*" -exec rm -rf {} \;
+    find ${complate_gopath_dir}/pkg/mod/github.com/george012  -name "${ProductName}@*" -exec rm -rf {} \;
 }
 
 
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package gtbox
 
-const VERSION = "v0.0.32"
+const VERSION = "v0.0.33"
