2486e96604c1bdab916a3af99226ee9b7af7a82b
WMYeah_Mac <george012@163.com>
2023-07-11 19:10:56 +0800

    Update v0.0.64

--- git_tag.sh
+++ git_tag.sh
@@ -11,53 +11,71 @@ CurrentVersionString=`echo "${aVersion/'const VERSION = '/}" | sed 's/\"//g'`
 
 versionStr=""
 
-if [ -z "$1" ]; then
-    echo "============================ ${ProductName} ============================"
-    echo "  1、发布 [-${ProductName}-]"
-    echo "  当前版本[-${CurrentVersionString}-]"
-    echo "======================================================================"
-    read -p "$(echo -e "请输入版本号[例如；v0.0.1]")" inputString
-    if [[ "$inputString" =~ ^v.* ]]; then
-        versionStr=${inputString}
+function to_run() {
+    if [ -z "$1" ]; then
+        echo "============================ ${ProductName} ============================"
+        echo "  1、发布 [-${ProductName}-]"
+        echo "  当前版本[-${CurrentVersionString}-]"
+        echo "======================================================================"
+        read -p "$(echo -e "请输入版本号[例如；v0.0.1]")" inputString
+        if [[ "$inputString" =~ ^v.* ]]; then
+            versionStr=${inputString}
+        else
+            versionStr=v${inputString}
+        fi
+        return 0
+    elif [ "$1" == "auto" ]; then
+        baseStr=$(echo $CurrentVersionString | cut -d'.' -f1)     # Get the base version (v0)
+        base=${baseStr//v/}                                       # Get the base version (0)
+        major=$(echo $CurrentVersionString | cut -d'.' -f2)       # Get the major version (0)
+        minor=$(echo $CurrentVersionString | cut -d'.' -f3)       # Get the minor version (1)
+
+        minor=$((minor+1))                          # Increment the minor version
+        if ((minor==100)); then                     # Check if minor version is 100
+            minor=0                                 # Reset minor version to 0
+            major=$((major+1))                      # Increment major version
+        fi
+
+        if ((major==100)); then                     # Check if major version is 100
+            major=0                                 # Reset major version to 0
+            base=$((base+1))                        # Increment base version
+        fi
+
+        versionStr="v${base}.${major}.${minor}"
+        return 0
     else
-        versionStr=v${inputString}
+      return 1
     fi
-elif [ "$2" == "auto" ]; then
-    base=$(echo $CurrentVersionString | cut -d'.' -f1)      # Get the base version (v0)
-    major=$(echo $CurrentVersionString | cut -d'.' -f2)      # Get the major version (0)
-    minor=$(echo $CurrentVersionString | cut -d'.' -f3)      # Get the minor version (1)
+}
 
-    minor=$((minor+1))                          # Increment the minor version
-    if ((minor==100)); then                     # Check if minor version is 100
-        minor=0                                 # Reset minor version to 0
-        major=$((major+1))                      # Increment major version
-    fi
+function git_handle() {
+    fileVersionLineNo=`cat $VersionFile | grep -n "const VERSION =" | awk -F ":" '{print $1}'`
 
-    if ((major==100)); then                     # Check if major version is 100
-        major=0                                 # Reset major version to 0
-        base="${base}1"                         # Increment base version
-    fi
+    oldfileVersionStr=`cat $VersionFile | grep -n "const VERSION =" | awk -F ":" '{print $2}'`
 
-    versionStr="${base}.${major}.${minor}"
+    newVersionStr='const VERSION = ''"'$versionStr'"'
+
+    sed -i -e "${fileVersionLineNo}s/${oldfileVersionStr}/${newVersionStr}/g" $VersionFile
+
+    ovs=${oldfileVersionStr#const VERSION = \"}
+    APP_OLD_VERSION=${ovs%\"}
+    PRE_DEL_VERSION=${APP_OLD_VERSION%.*}.$((${APP_OLD_VERSION##*.}-1))
+
+    git add . \
+    && git commit -m "Update ${versionStr}"  \
+    && git tag $versionStr \
+    && git push \
+    && git push --tags \
+    && git tag -f latest $versionStr \
+    && git push -f origin latest \
+    && git tag -d $PRE_DEL_VERSION
+}
+
+
+if to_run "$1"; then
+    git_handle
+else
+    echo "Invalid argument"
 fi
 
-fileVersionLineNo=`cat $VersionFile | grep -n "const VERSION =" | awk -F ":" '{print $1}'`
 
-oldfileVersionStr=`cat $VersionFile | grep -n "const VERSION =" | awk -F ":" '{print $2}'`
-
-newVersionStr='const VERSION = ''"'$versionStr'"'
-sed -i.bak -e "${fileVersionLineNo}s/${oldfileVersionStr}/${newVersionStr}/g" $VersionFile
-rm -rf $VersionFile.bak
-
-ovs=${oldfileVersionStr#const VERSION = \"}
-APP_OLD_VERSION=${ovs%\"}
-PRE_DEL_VERSION=${APP_OLD_VERSION%.*}.$((${APP_OLD_VERSION##*.}-1))
-
-git add . \
-&& git commit -m "Update ${versionStr}"  \
-&& git tag $versionStr \
-&& git push \
-&& git push --tags \
-&& git tag -f latest $versionStr \
-&& git push -f origin latest \
-&& git tag -d $PRE_DEL_VERSION
